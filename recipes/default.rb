68983af5bf077220a1085f3b5f60b2c4eb9985f6
tkuchiki <kuchiki.taku@gmail.com>
2014-04-10 21:10:36 +0900

    Setting default_configure_options.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,10 @@
-git_action     = node[:php_build][:git_action]
-git_revision   = node[:php_build][:git_revision]
-install_dir    = "#{node[:php_build][:prefix]}/php-build"
-prefix         = node[:php_build][:prefix]
-php_build_repo = node[:php_build][:repository]
+git_action      = node[:php_build][:git_action]
+git_revision    = node[:php_build][:git_revision]
+prefix          = node[:php_build][:prefix]
+install_dir     = "#{prefix}/php-build"
+php_build_repo  = node[:php_build][:repository]
+config_dir      = node[:php_build][:config_dir]
+default_options = "#{config_dir}/default_configure_options"
 
 git install_dir do
   action     git_action.to_sym
@@ -17,4 +19,8 @@ bash "exec install.sh" do
 EOC
 
   not_if "which php-build"
+end
+
+template default_options do
+  source "default_configure_options.erb"
 end
